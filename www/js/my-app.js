26b592ac425514014654a56ef8d57fb024e4715b
Bynnes <falecom@bynn.es>
2017-10-24 19:22:48 -0300

    config

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -10159,7 +10159,7 @@ function limpar()
                 },
                 ios: {
                     senderID: "10572585588",
-                    gcmSandbox: "true", // false para producao true para desenvolvimento
+                    gcmSandbox: "false", // false para producao true para desenvolvimento
                     alert: "true",
                     sound: "true",
                     badge: "false"
